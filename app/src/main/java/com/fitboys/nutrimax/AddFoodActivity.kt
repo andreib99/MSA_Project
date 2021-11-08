f19daf1d148df1c2be28e598d4014d0df416edd2
Your Name <you@example.com>
2021-11-08 08:28:32 +0200

    Added Email verification system and work on home actibity

---
+++ app/src/main/java/com/fitboys/nutrimax/AddFoodActivity.kt
@@ -0,0 +1,13 @@
+package com.fitboys.nutrimax
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddFoodActivity : AppCompatActivity()  {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_food)
+    }
+}
