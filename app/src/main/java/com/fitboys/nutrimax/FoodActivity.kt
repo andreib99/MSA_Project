16efd7a8b2cc304b5f4c9acedfbae268d4d7216f
Your Name <you@example.com>
2021-12-21 19:01:00 +0200

    Started working on food page details

---
+++ app/src/main/java/com/fitboys/nutrimax/FoodActivity.kt
@@ -0,0 +1,33 @@
+package com.fitboys.nutrimax
+
+import HomeFragment
+import ProfileFragment
+import SettingsFragment
+import android.content.ContentValues
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+
+class FoodActivity : AppCompatActivity()  {
+    private var mAuth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food)
+        val b = intent.extras
+        mAuth = FirebaseAuth.getInstance()
+        var foodName: TextView = findViewById(R.id.etName)
+        if (b != null) {
+            Log.d(ContentValues.TAG, "Food name = ${b.getString("name")}")
+            foodName.text = b.getString("name")
+        }
+
+    }
+}
--- app/src/main/java/com/fitboys/nutrimax/FoodAdapter.kt
+++ app/src/main/java/com/fitboys/nutrimax/FoodAdapter.kt
@@ -1,6 +1,8 @@
 package com.fitboys.nutrimax
 
 import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
 
 import android.util.Log
@@ -10,7 +12,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.fitboys.nutrimax.data.model.Food
 import android.view.LayoutInflater
+import android.widget.AdapterView
 import android.widget.ImageView
+import androidx.core.content.ContextCompat.startActivity
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.storage.FirebaseStorage
@@ -18,7 +22,7 @@ import java.io.File
 import java.io.IOException
 
 
-class FoodAdapter(options: FirestoreRecyclerOptions<Food>) : FirestoreRecyclerAdapter<
+class FoodAdapter(options: FirestoreRecyclerOptions<Food>, private val listener: OnItemClickListener) : FirestoreRecyclerAdapter<
         Food, FoodAdapter.foodsViewholder>(options) {
 
 
@@ -30,34 +34,45 @@ class FoodAdapter(options: FirestoreRecyclerOptions<Food>) : FirestoreRecyclerAd
     }
 
     override fun onBindViewHolder(holder: foodsViewholder, position: Int, model: Food) {
-        holder.name.text = model.name
-        holder.quantity.text = model.quantity.toString()
-        holder.calories.text = model.calories.toString()
-        holder.rating.text = model.rating.toString()
-        holder.image_path.text = model.image
-        val mStoreReference = FirebaseStorage.getInstance().reference
-            .child("images/${holder.image_path.text}")
-        Log.d(ContentValues.TAG, "image = ${holder.image_path.text}")
-        try {
-            val localFile = File.createTempFile("food", "jpg")
-            mStoreReference.getFile(localFile)
-                .addOnSuccessListener {
-                    val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
-                    (holder.image).setImageBitmap(bitmap)
-                }.addOnFailureListener { }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
+            holder.name.text = model.name
+            holder.quantity.text = model.quantity.toString()
+            holder.calories.text = model.calories.toString()
+            holder.rating.text = model.rating.toString()
+            holder.image_path.text = model.image
+            val mStoreReference = FirebaseStorage.getInstance().reference
+                .child("images/${holder.image_path.text}")
+            Log.d(ContentValues.TAG, "image = ${holder.image_path.text}")
+            try {
+                val localFile = File.createTempFile("food", "jpg")
+                mStoreReference.getFile(localFile)
+                    .addOnSuccessListener {
+                        val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+                        (holder.image).setImageBitmap(bitmap)
+                    }.addOnFailureListener { }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
     }
 
-    class foodsViewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class foodsViewholder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
         var name: TextView = itemView.findViewById(R.id.name)
         var quantity: TextView = itemView.findViewById(R.id.quantity)
         var calories: TextView = itemView.findViewById(R.id.calories)
         var rating: TextView = itemView.findViewById(R.id.rating)
         var image: ImageView = itemView.findViewById(R.id.FoodImage)
         var image_path: TextView = itemView.findViewById(R.id.image)
+        init {
+            itemView.setOnClickListener(this)
+        }
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position, this.name.text as String)
+            }
+        }
+    }
 
+    interface OnItemClickListener {
+        fun onItemClick(position: Int, name: String)
     }
 }
